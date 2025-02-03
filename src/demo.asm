504239944b3bb3c0e26a7da3ad40b58bc32e9ccf
Ties Stuij <ties@stuij.se>
2025-02-03 01:24:21 +0000

    load mode 1 bg2, so using tiles and palette
    Yay, wrote my first proper 65816 assembly code!

--- src/demo.asm
+++ src/demo.asm
@@ -1,9 +1,6 @@
 .p816   ; 65816 processor
 .smart
 
-.i16    ; X/Y are 16 bits
-.a8     ; A is 8 bits
-
 .define ROM_NAME "demo"
 .include "meta-data.inc"
 .include "macros.inc"
@@ -15,8 +12,16 @@
 
 .code
 
+.i16    ; X/Y are 16 bits
+.a8     ; A is 8 bits
 reset:
-    init_cpu
+    clc
+    xce
+	; FYI: coming out of emulation mode, the M and X bits of the
+	; status registers are set to one. So resp. A and X/Y
+	; are set to 8 bit.
+	setXY16
+    setA8
 
     ; Clear PPU registers
     ldx #$33
@@ -25,11 +30,74 @@ reset:
     dex
     bpl @loop
 
-    ; Set background color to $03E0
-    lda #$E0
+    lda #$80
+    sta INIDISP ; Turn off screen (forced blank)
+
+    jmp main
+
+
+VRAM_MAP_BASE = $0000
+VRAM_CHR_BASE = $1000
+
+.i16
+.a8
+main:
+    ;; set up bg registers
+    lda #1 ; mode 1
+    sta BGMODE
+    lda #(VRAM_MAP_BASE >> 10) ; still 0
+    sta BG1SC ; set bg 2 tile map
+    ; set bg 2 char vram base addr (implicitly setting bg1 to 0, but we don't
+    ; care)
+    lda #((VRAM_CHR_BASE >> 12) << 4)
+    sta BG12NBA
+
+    ; let's copy over some binary data
+    ;; first the palette
+    stz CGADD ; set to begin of palette mem
+    ldx #(town_palette_end - town_palette) ; # of palette entries
+    ldy #0
+@palette_loop:
+    lda town_palette, y
     sta CGDATA
-    lda #$03
-    sta CGDATA
+    iny
+    dex
+    bne @palette_loop
+
+    lda #$80
+    sta VMAIN
+
+    ; load map
+    ldx #VRAM_MAP_BASE
+    stx VMADDL
+    ldx #0
+@map_loop:
+     lda town_map,x
+     sta VMDATAL
+     inx
+     lda town_map,x
+     sta VMDATAH
+     inx
+     cpx #(town_map_end - town_map)
+     bne @map_loop
+
+    ; load tiles
+    ldx #VRAM_CHR_BASE
+    stx VMADDL
+    ldx #0
+@char_loop:
+     lda town_tiles,x
+     sta VMDATAL
+     inx
+     lda town_tiles,x
+     sta VMDATAH
+     inx
+     cpx #(town_tiles_end - town_tiles)
+     bne @char_loop
+
+    ; turn on BG2
+    lda #2
+    sta TM
 
     ; Maximum screen brightness
     lda #$0F
@@ -37,3 +105,17 @@ reset:
 
 forever:
     jmp forever
+
+.segment "RODATA"
+
+town_tiles:
+.incbin"assets/town-256.tiles"
+town_tiles_end:
+
+town_palette:
+.incbin"assets/town-256.palette"
+town_palette_end:
+
+town_map:
+.incbin"assets/town-256.map"
+town_map_end:
